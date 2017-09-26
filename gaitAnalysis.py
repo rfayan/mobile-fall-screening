32ef9c73ac2b01ee51bd11eddf5aa84f274a0d85
patriciabet <patriciabet95@gmail.com>
2017-09-26 19:48:55 -0300

    Update gaitAnalysis.py

--- gaitAnalysis.py
+++ gaitAnalysis.py
@@ -2,6 +2,7 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+
 from numpy import genfromtxt
 
 data =genfromtxt('C:\\Users\\patricia\\Desktop\\Dados Acelerômetro\\001_Accelerometer_20170719-161640829.csv', delimiter=',')
@@ -11,14 +12,13 @@ mat=np.bmat(lst)
 mat=np.reshape(mat,(N,4))
 
 plt.plot(mat[:,1])
-[<matplotlib.lines.Line2D object at 0x000001504761D588>]
 plt.plot(mat[:,2])
-[<matplotlib.lines.Line2D object at 0x000001504762F4E0>]
 plt.plot(mat[:,3])
-[<matplotlib.lines.Line2D object at 0x000001504762FD68>]
 plt.show()
 
 matsquare=np.square(mat[:,1])+np.square(mat[:,2])+np.square(mat[:,3])
 matfusao=np.sqrt(matsquare)
+
+
 
 
