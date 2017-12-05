4342f7fcf92d77a3ac77a1914c6879777779fd3f
Moacir A Ponti <ponti@usp.br>
2017-12-05 17:09:49 -0200

    Corrigida extracao de caracteristicas

--- gaitAnalysis.py
+++ gaitAnalysis.py
@@ -2,13 +2,20 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import re
+
+import scipy
+import scipy.fftpack
+import pylab
+from scipy import pi
+from scipy.signal import butter, lfilter, freqz
 
 from numpy import genfromtxt
 from matplotlib.backends.backend_pdf import PdfPages
 
 
 # Moacir
-#directory = '/home/maponti/Repos/mobile-fall-screening/data/'
+directory = '/home/maponti/Repos/mobile-fall-screening/data/'
 
 #Patricia
 #directory = 'C:\\Users\\Patrícia Bet\\Desktop\\Dados Acelerômetro\\
@@ -70,11 +77,9 @@ def createMatrixFromCsv(directory = 'C:\\Users\\Patrícia Bet\\Desktop\\Dados Ac
                                        
 
 
-def espPot (directory = 'C:\\Users\\Patrícia Bet\\Desktop\\Dados Acelerômetro\\'):
+def espPot (directory = 'C:\\Users\\Patrícia Bet\\Desktop\\Dados Acelerômetro\\', filtering=False):
 
     """Function to generate pdf of the axes fusion, power spectrum and the logarithm of the power spectrum"""
-
-    j=1
 
     for root, dirs, files in os.walk(directory):
         for f in files:
@@ -83,6 +88,11 @@ def espPot (directory = 'C:\\Users\\Patrícia Bet\\Desktop\\Dados Acelerômetro\
                 # file name
                 f = directory+f
                 print(f)
+
+                # pega o ultimo elemento apos particionar com \ ou /
+                # desse pega os tres primeiros valores
+                # e depois converte para inteiro
+                j = int(re.split('\\ |/', f)[-1][0:3]) 
 
                 data = genfromtxt(f,delimiter=',')
                 lst = [elem for elem in data]
@@ -97,10 +107,17 @@ def espPot (directory = 'C:\\Users\\Patrícia Bet\\Desktop\\Dados Acelerômetro\
                 # convert array of arrays to single array
                 matFusao = np.squeeze(np.asarray(matFusao[1:]))
                 dataFftS = np.fft.fft(matFusao)
-                espPot = np.abs(dataFftS)**2
+                espPotS = np.abs(dataFftS)**2
+
+                # filtering
+                if filtering == True:
+                    fs = 50
+                    matFusaoF = butter_lowpass_filter(matFusao, 3.6667, fs, order=6)
+                    dataFftF = np.fft.fft(matFusaoF)
+                    espPotF = np.abs(dataFftF)**2
 
                 # number of frequencies to be displayed
-                n = espPot.size
+                n = espPotS.size
                 nhalf = int(n/2)
                 maxfr = 100
                 timestep = 0.1
@@ -111,29 +128,34 @@ def espPot (directory = 'C:\\Users\\Patrícia Bet\\Desktop\\Dados Acelerômetro\
                 # signal fusion
                 ax1 = fig.add_subplot(311)
                 ax1.plot(matFusao)
+                if filtering:
+                    ax1.plot(matFusaoF)
                 #ax1.set_title('Signal Fusion')
 
                 # power spectrum
                 ax2 = fig.add_subplot(312)
-                ax2.plot(espPot[1:maxfr]+1)
+                ax2.plot(espPotS[1:maxfr]+1)
+                if filtering:
+                    ax2.plot(espPotF[1:maxfr]+1)
                 #ax2.set_title('Power Spectrum')
 
                 # logarithm of the power spectrum
                 ax3 = fig.add_subplot(313)
-                ax3.plot(np.log(espPot[1:maxfr]+1))
+                ax3.plot(np.log(espPotS[1:maxfr]+1))
+                if filtering:
+                   ax3.plot(np.log(espPotF[1:maxfr]+1))
                 #ax3.set_title('Logarithm of the Power Spectrum')
 
                 nome = "dados%03d.pdf"%j
                 pp = PdfPages(nome)
                 pp.savefig()
                 pp.close()
-                plt.show()
-                plt.clf()
-                j=j+1
+                #plt.show()
+                #plt.clf()
 
 
 
-def featuresAcc (directory = 'C:\\Users\\Patrícia Bet\\Desktop\\Dados Acelerômetro\\'):
+def featuresAcc (directory = 'C:\\Users\\Patrícia Bet\\Desktop\\Dados Acelerômetro\\', filtering=False):
 
     """Function to ... """
 
@@ -147,6 +169,12 @@ def featuresAcc (directory = 'C:\\Users\\Patrícia Bet\\Desktop\\Dados Acelerôm
                 f = directory + f
                 print(f)
 
+                # pega o ultimo elemento apos particionar com \ ou /
+                # desse pega os tres primeiros valores
+                # e depois converte para inteiro
+                j = int(re.split('\\ |/', f)[-1][0:3]) 
+
+
                 data = genfromtxt(f,delimiter=',')
                 lst = [elem for elem in data]
                 N = len(lst)
@@ -159,13 +187,19 @@ def featuresAcc (directory = 'C:\\Users\\Patrícia Bet\\Desktop\\Dados Acelerôm
 
                 # convert array of arrays to single array
                 matFusao = np.squeeze(np.asarray(matFusao[1:]))
-                dataFftS = np.fft.fft(matFusao)
+
+                # filtering
+                if filtering == True:
+                    fs = 75
+                    matFusao = butter_lowpass_filter(matFusao, 3.667 , fs, order=6)
+
+                dataFftS = np.fft.fft(matFusao[5:])
                 espPot = np.abs(dataFftS)**2
 
                 # number of frequencies to be displayed
                 n = espPot.size
                 nhalf = int(n/2)
-                maxfr = 100
+                maxfr = 50
                 timestep = 0.1
                 freq = np.fft.fftfreq(n,timestep)
 
@@ -194,26 +228,29 @@ def featuresAcc (directory = 'C:\\Users\\Patrícia Bet\\Desktop\\Dados Acelerôm
                 id_vol = "vol_%03d"%j
                 months = -1
 
-                featMatrix = [id_vol, features, months]
+                #featMatrix = [id_vol, features, months]
+                                
+                featMatrix.append([id_vol] + features + [months])
+
                 
-                
-                #featMatrix.append([id_vol] + features + [months])
-                
-                #featMatrix = np.array(featMatrix)
-                
- 
-    
+    featMatrix = np.array(featMatrix)
     return featMatrix
 
-                #### end of data reading
 
 
-                # featMatrix will be a list
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
 
-                #featMatrix = np.array(featMatrix)
-                
-                
-                # show all features psp (column 2, index 1)   
-                #print(featMatrix[:, 1])
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
+
 
 
