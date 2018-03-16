8400eb1395266a9a15c1ed75030e7bcfe4299bd4
Moacir A Ponti <ponti@usp.br>
2018-03-16 09:21:48 -0300

    Inicio da implementacao da segmentacao

--- gaitAnalysis.py
+++ gaitAnalysis.py
@@ -15,14 +15,18 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 
 # Moacir
-#directory = '/home/maponti/Repos/mobile-fall-screening/data/'
+directory = '/home/maponti/Repos/mobile-fall-screening/data/'
 
 # listas de caidores e excluidos
 index_faller = [9, 10, 35, 40, 59, 70, 77]
 index_excluded = [5, 16, 26, 48, 65, 66]
 
 # dicionario com indices e meses
+# podemos usar meses, e depois se precisar convertemos tudo para '1' (caidor)
 dict_label = {7:3, 9:1, 10:1, 35:1, 40:1, 59:1, 70:1, 77:1}
+
+# dicionario com o numero de quedas
+dict_qtde  = {7:1, 9:1, 10:1, 35:1, 40:1, 59:1, 70:1, 77:1}
 
 #Patricia
 #directory = 'C:\\Users\\Patrícia Bet\\Desktop\\Dados Acelerômetro\\
@@ -159,6 +163,87 @@ def espPot (directory = 'C:\\Users\\Patrícia Bet\\Desktop\\Dados Acelerômetro\
                 pp.close()
                 #plt.show()
                 #plt.clf()
+
+
+
+def segmentTUGs(directory = 'C:\\Users\\Patrícia Bet\\Desktop\\Dados Acelerômetro\\', filtering=False, sumFilterSize=3):
+
+    ''' Function to segment the different TUGs along the signal '''
+    for root, dirs, files in os.walk(directory):
+        for f in files:
+            if f.endswith(".csv"):
+
+                # file name
+                f = directory+f
+                print(f)
+
+                # pega o ultimo elemento apos particionar com \ ou /
+                # desse pega os tres primeiros valores
+                # e depois converte para inteiro
+                j = int(re.split('\\ |/', f)[-1][0:3]) # Linux
+                #j = int(re.split('\\\\', f)[-1][0:3]) # Windows
+
+                data = genfromtxt(f,delimiter=',')
+                lst = [elem for elem in data]
+                N = len(lst)
+                mat = np.bmat(lst)
+                mat = np.reshape(mat,(N,4))
+
+                # signal fusion = sqrt( x^2 + y^2 + z^2 )
+                matSquare = np.square(mat[:,1])+np.square(mat[:,2])+np.square(mat[:,3])
+                matFusao = np.sqrt(matSquare)
+
+                # convert array of arrays to single array
+                matFusao = np.squeeze(np.asarray(matFusao[1:]))
+                matFusao[0:5] = 1
+
+                # filtro para realizar convolucao
+                # h = [1, 1, 1]
+                h = np.ones(sumFilterSize)
+                matSegm = np.convolve(matFusao, h, mode='same')
+    
+                matSegm = (matSegm-np.mean(matSegm)) / np.std(matSegm)
+
+                matSegm[ : int(sumFilterSize/2)] = 0
+                matSegm[-int(sumFilterSize/2) : ] = 0
+        
+                # especificar um limiar e pegar apenas valores acima
+                mask = np.zeros(matSegm.shape)
+                mask[np.where(matSegm > 0)] = 1
+
+                print(mask)
+
+                dataFftS = np.fft.fft(matFusao)
+                espPotS = np.abs(dataFftS)**2
+
+                # filtering
+                if filtering == True:
+                    fs = 50
+                    matFusaoF = butter_lowpass_filter(matFusao, 3.6667, fs, order=6)
+                    dataFftF = np.fft.fft(matFusaoF)
+                    espPotF = np.abs(dataFftF)**2
+
+                fig = plt.figure()
+
+                # signal fusion
+                ax1 = fig.add_subplot(211)
+                ax1.plot(matFusao)
+                if filtering:
+                    ax1.plot(matFusaoF*mask)
+                #ax1.set_title('Signal Fusion')
+
+                # segmentation
+                ax2 = fig.add_subplot(212)
+                ax2.plot(matSegm)
+                ax2.plot(mask)
+
+                nome = "dados_segmentacao%03d.pdf"%j
+                pp = PdfPages(nome)
+                pp.savefig()
+                pp.close()
+                #plt.show()
+                plt.clf()
+                
 
 
 
